d8338f3ad2372c1d18739e2a2bb5bbddb530025a
Mister Black <misterblack@example.com>
2022-07-04 16:22:51 +1000

    Intial Anisible Box

---
+++ ansible.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#This script installs anisble to the VM
+
+apt-get update
+apt-get install -y ansible
---
+++ python.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+#This script installs python to the VM
+
+apt-get update
+apt-get install -y python
